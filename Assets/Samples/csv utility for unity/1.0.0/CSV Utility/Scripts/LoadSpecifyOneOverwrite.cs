8bc14a0471aa489b3db5ff3d9b02a6eac76c0a23
边上海 <bshsf@qq.com>
2023-08-12 23:04:08 +0800

    更新了 LoadSpecifyOneOverwrite 示例代码，方便调整 filter 数值进行测试

--- Assets/Samples/csv utility for unity/1.0.0/CSV Utility/Scripts/LoadSpecifyOneOverwrite.cs
+++ Assets/Samples/csv utility for unity/1.0.0/CSV Utility/Scripts/LoadSpecifyOneOverwrite.cs
@@ -5,6 +5,7 @@ namespace zFramework.Examples
 {
     public class LoadSpecifyOneOverwrite : Base
     {
+        public int a = 7;
         public override string Title { get; } = "Load";
         public override string Description => IsChineseUser ?
             "加载指定的某一行，在本例，加载 a = 1 这一行数据到已存在的 A 对象" :
@@ -12,20 +13,20 @@ namespace zFramework.Examples
         public override void Execute()
         {
             var file = Path.Combine(Application.persistentDataPath, "la.csv");
-            var csv = "a,bb,c,d,e\n1,2,3.1,true,11\n4,5,6.1,false,22\n7,8,9.0,true,33";
+            var csv = "a,bb,c,d,e\n1,2,3.1,true,11\n4,5,6.1,false,22\n7,8,9.0,true,33\n7,81,9.1,true,331";
             File.WriteAllText(file, csv);
 
             // test load specify one where a equal 1
             // will load the data where a = 7 into the exist A instance
-            //  e must equal default value 11, because it is ignored
+            //  e must equal default value 111, because it is ignored
             // others will be overwrite
             var a = new A
             {
-                a = 7,
-                b = "2",
-                c = 3.1f,
+                a = this.a,
+                b = "22",
+                c = 3.11f,
                 d = true,
-                e = 11
+                e = 111
             };
             Debug.Log($"Before： a = {a.a} , b = {a.b}, c = {a.c} , d = {a.d}, e = {a.e} ");
             var cached = a;
